402e8ce38816bcad00510ebaf64c82ec349e7bab
appcreatorguy <manmenonsense@gmail.com>
2021-08-25 19:21:43 +0100

    Initial commit

---
+++ src/main/java/com/github/appcreatorguy/mods/sidewaysblocks/sidewaysblocks/Sidewaysblocks.java
@@ -0,0 +1,10 @@
+package com.github.appcreatorguy.mods.sidewaysblocks.sidewaysblocks;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Sidewaysblocks implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/github/appcreatorguy/mods/sidewaysblocks/sidewaysblocks/client/SidewaysblocksClient.java
@@ -0,0 +1,13 @@
+package com.github.appcreatorguy.mods.sidewaysblocks.sidewaysblocks.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class SidewaysblocksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
